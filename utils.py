6031600e211774724d9c98d865e6cf57912736f8
Prudhvi Kommareddi <pvkommareddi@mun.ca>
2021-06-17 17:17:26 +0530

    Add method for creating temp dir

--- utils.py
+++ utils.py
@@ -22,15 +22,18 @@ def save_scenarios_as_delimited_text(scenarios):
     write_delimited_text(rcps, 'rcps.tsv', delimiter='\t')
 
 
+def create_temp_dir_if_not_exists():
+    if not os.path.exists('temp'):
+        os.makedirs('temp')
+
+
 def write_delimited_text(data: MAGICCData, filename, delimiter=',', header=True, index=False, append=False):
     '''
     Write a MAGICCData object as a delimited text file to disk
     '''
 
-    if not os.path.exists('temp'):
-        os.makedirs('temp')
+    create_temp_dir_if_not_exists()
     filename = os.path.join('temp', filename)
-
     mode = 'a' if append else 'w'
     data = convert_MAGICCData_to_df(data)
     data.to_csv(filename, sep=delimiter, header=header, index=index, mode=mode)
