9355f2a72b2a68f7882e3c337780370c2e89f533
Prudhvi Kommareddi <pvkommareddi@mun.ca>
2021-06-17 14:49:44 +0530

    add some generic methods

--- utils.py
+++ utils.py
@@ -1,7 +1,25 @@
 import os
 
 import pandas as pd
+from pandas import DataFrame
 from pymagicc.io import MAGICCData
+
+from copy import deepcopy
+
+
+def convert_MAGICCData_to_df(data: MAGICCData) -> DataFrame:
+    return data.timeseries().reset_index()
+
+
+def convert_df_to_MAGICCData(data: DataFrame) -> MAGICCData:
+    return MAGICCData(data)
+
+
+def save_scenarios_as_delimited_text(scenarios):
+    rcps = deepcopy(scenarios[0])
+    for scenario in scenarios[1:]:
+        rcps.append(scenario, inplace=True)
+    write_delimited_text(rcps, 'rcps.tsv', delimiter='\t')
 
 
 def write_delimited_text(data: MAGICCData, filename, delimiter=',', header=True, index=False, append=False):
@@ -14,7 +32,7 @@ def write_delimited_text(data: MAGICCData, filename, delimiter=',', header=True,
     filename = os.path.join('temp', filename)
 
     mode = 'a' if append else 'w'
-    data = data.timeseries().reset_index()
+    data = convert_MAGICCData_to_df(data)
     data.to_csv(filename, sep=delimiter, header=header, index=index, mode=mode)
 
 
